548a72f6d55179695d44cc11ecc62bc51d47714f
danalvarez <d.alvrzx@gmail.com>
2015-10-13 17:36:43 -0600

    Primera versión: ArbolBiselado

---
+++ src/ArbolBiseladoTest.java
@@ -0,0 +1,38 @@
+import static org.junit.Assert.*;
+
+/*
+UVG
+Algoritmos y Estructuras de Datos - 2011
+Hoja de trabajo 7
+Autor: Eduardo Castellanos
+
+Descripción:JUnit test para SplayTree
+*/
+
+import org.junit.Test;
+
+public class ArbolBiseladoTest {
+
+	@Test
+	public void testAdd() {
+		MapaPicadillo obj = new MapaPicadillo();
+		Word palabra = new Word("Correr","Verbo");
+		obj.add(palabra);
+		if(!obj.get(palabra).equals(palabra)){
+			
+			fail("Error");
+		}
+	}
+	public void testGet(){
+		MapaPicadillo obj = new MapaPicadillo();
+		Word palabra = new Word("Correr","Verbo");
+		obj.add(palabra);
+		try{
+			palabra = (Word) obj.get(palabra);
+		}
+		catch(ClassCastException e){
+			fail("Error");
+		}
+	}
+
+}
