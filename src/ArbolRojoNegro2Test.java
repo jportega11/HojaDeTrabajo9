b186a8ce3df5761adccc870a2346b5c663546227
danalvarez <d.alvrzx@gmail.com>
2015-10-13 17:38:04 -0600

    Primera versión: ArbolRojoNegro2

---
+++ src/ArbolRojoNegro2Test.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ArbolRojoNegro2Test {
+
+	@Test
+	public void testAdd() {
+		MapaPicadillo obj = new MapaPicadillo();
+		Word palabra = new Word("Correr","Verbo");
+		obj.add(palabra);
+		if(!obj.get(palabra).equals(palabra)){
+			
+			fail("Error");
+		}
+	}
+	public void testGet(){
+		MapaPicadillo obj = new MapaPicadillo();
+		Word palabra = new Word("Correr","Verbo");
+		obj.add(palabra);
+		try{
+			palabra = (Word) obj.get(palabra);
+		}
+		catch(ClassCastException e){
+			fail("Error");
+		}
+	}
+}
